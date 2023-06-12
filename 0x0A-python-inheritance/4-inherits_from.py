a918e2f394f38c3faf4e667faf1f7762d5e6cd14
Wandile-cyber <ralphdlamini3@gmail.com>
2023-06-12 12:10:32 -0700

    inheritance corrections

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-'''function that returns True if the object is an instance of a class
-   that inherited (directly or indirectly) from the specified class ;
-   otherwise False.
-'''
+"""Defines class checking"""
 
 
 def inherits_from(obj, a_class):
-    '''
-    Returns True or False
-    '''
-    return isinstance(obj, a_class) and not isinstance(obj, a_class)
+    """Checking the right instance
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an instance or inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    if isinstance(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
