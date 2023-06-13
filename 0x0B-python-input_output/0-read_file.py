1f71a5e523f158df1479608afccb6e4fe5ab3207
Wandile-cyber <ralphdlamini3@gmail.com>
2023-06-13 10:27:03 -0700

    checking

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-'''Python Function that reads a file and prints to stdout'''
 
 
 def read_file(filename=""):
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
-            print(line, end="")
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
