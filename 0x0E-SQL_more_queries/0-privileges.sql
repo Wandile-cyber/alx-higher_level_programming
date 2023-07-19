764a50d51c12bd5e2065ed7f44e07ec1e99594fe
Wandile-cyber <ralphdlamini3@gmail.com>
2023-07-19 11:53:17 -0700

    more queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- MySql Script to list all privileges for MqSql users
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- List all privileges for users 'user_0d_1' and 'user_0d_2'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
