d0a6994a4d47b0ae46a832d208c5c320b9f86d59
Wandile-cyber <ralphdlamini3@gmail.com>
2023-08-15 17:15:25 +0000

    my commiting

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destinationFile = process.argv[4];
+
+const file1Content = fs.readFileSync(sourceFile1, 'utf-8');
+const file2Content = fs.readFileSync(sourceFile2, 'utf-8');
+
+const concatenatedContent = file1Content + file2Content;
+
+fs.writeFileSync(destinationFile, concatenatedContent);
+
+console.log(`Files ${sourceFile1} and ${sourceFile2} have been concatenated into ${destinationFile}`);
