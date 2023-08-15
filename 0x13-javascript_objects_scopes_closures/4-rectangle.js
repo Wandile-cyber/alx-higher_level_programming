b419b80bb4d14383de03fe94b59fc0fdf27c77f9
Wandile-cyber <ralphdlamini3@gmail.com>
2023-08-15 17:22:57 +0000

    check

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -9,16 +9,19 @@ class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let count = 0; count < this.height; count++) {
       console.log('X'.repeat(this.width));
     }
   }
+
   rotate () {
-    let holder = this.width;
+    const holder = this.width;
     this.width = this.height;
     this.height = holder;
   }
+
   double () {
     this.width = 2 * this.width;
     this.height = 2 * this.height;
