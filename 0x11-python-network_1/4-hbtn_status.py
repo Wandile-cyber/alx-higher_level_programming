5823632964d8d8fee65f72f730d1dd98d4d72cff
Wandile-cyber <ralphdlamini3@gmail.com>
2023-09-01 16:13:26 -0700

    corrections

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-# Python script that fetches a url using only requests
+"""
+Script to check status
+"""
+
+
 if __name__ == "__main__":
-    import requests as req
-    r = req.get('https://intranet.hbtn.io/status')
-    print('Body response:\n\t- type: {}\n\t- content: {}'
-          .format(type(r.text), r.text))
+    import requests
+
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-# Python script that fetches a url and get header value using only requests
+"""
+Python script for requests
+"""
+
+
 if __name__ == "__main__":
-    import requests as req
+    import requests
     from sys import argv
 
-    r = req.get(argv[1])
-    print(r.headers.get('X-Request-Id'))
+    try:
+        req = requests.get(argv[1])
+        print(req.headers['X-Request-Id'])
+    except KeyError:
+        pass
