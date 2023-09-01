9b38fe512eed523173947f67c6478d9fc53a8d96
Wandile-cyber <ralphdlamini3@gmail.com>
2023-09-01 16:05:36 -0700

    0-htbn

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-# Python script that fetches a url
-import urllib.request
+"""
+Python script
+"""
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    print("Body response:")
-    print(f"\t- type: {type(response.read())}")
-    print(f"\t- content: {response.read()}")
-    print(f"\t- utf8 content: {response.read().decode()}")
+
+if __name__ == '__main__':
+    from urllib.request import Request, urlopen
+
+    req = Request('https://intranet.hbtn.io/status')
+    with urlopen(req) as f:
+        the_page = f.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(the_page)))
+        print('\t- content: {}'.format(the_page))
+        print('\t- utf8 content: {}'.format(the_page.decode('utf8')))
