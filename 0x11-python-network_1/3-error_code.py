ec38b6f22407a560dddb199bb4c1b8561588b5cd
Wandile-cyber <ralphdlamini3@gmail.com>
2023-09-01 16:10:36 -0700

    3 codes

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-# Python script to send request to URL and display error code
-if __name__ == "__main__":
-    import urllib.request as ur
-    import urllib.error as ure
-    from sys import argv
-    req = ur.Request(argv[1])
+"""
+Python script
+"""
 
+
+if __name__ == '__main__':
+    from urllib.request import Request, urlopen
+    from urllib.error import HTTPError
+    from sys import argv
+
+    req = Request(argv[1])
     try:
-        with ur.urlopen(req) as response:
-            print(str(response.read(), 'utf-8'))
-    except ure.HTTPError as err:
-        print('Error code: {}'.format(err.getcode()))
+        with urlopen(req) as f:
+            response = f.read()
+            print(response.decode('utf8'))
+    except HTTPError as e:
+        print("Error code:", e.code)
