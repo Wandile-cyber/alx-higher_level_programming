6360aa66fb033c4dcadde66b17fd55b67968611a
Wandile-cyber <ralphdlamini3@gmail.com>
2023-09-01 16:09:25 -0700

    second file

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-# Python script to PORT to URL
-if __name__ == "__main__":
-    import urllib.request as ur
-    import urllib.parse as urp
-    from sys import argv
+"""
+POST script
+"""
 
-    value = {'email': argv[2]}
-    data = urp.urlencode(value)
-    data = data.encode('ascii')  # data should be bytes
-    req = ur.Request(argv[1], data)
-    with ur.urlopen(req) as response:
-        print(str(response.read(), 'utf-8'))
+
+if __name__ == "__main__":
+    from urllib.request import Request, urlopen
+    from sys import argv
+    import urllib.parse
+
+    url = argv[1]
+    values = {'email': argv[2]}
+    data = urllib.parse.urlencode(values)
+    email = data.encode('ascii')
+    req = Request(url, email)
+    with urlopen(req) as response:
+        body = response.read()
+        print(body.decode('utf8'))
