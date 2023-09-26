fe2ff850c5fd8e4a581b5519c17bf812be5a6c00
Wandile-cyber <ralphdlamini3@gmail.com>
2023-09-26 05:36:13 -0700

    web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-let fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
