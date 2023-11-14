519eee065edff752fd9578cfdc5a4561035f5e15
Wandile-cyber <ralphdlamini3@gmail.com>
2023-11-14 11:20:26 -0800

    scopes

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,28 +1,8 @@
 #!/usr/bin/node
-#!/usr/bin/node
-
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
 const fs = require('fs');
-
-const srcFile1 = process.argv[2]; 
-const srcFile2 = process.argv[3];
-const destFile = process.argv[4];
-
-fs.writeFile(destFile, '', function (err) {
-  if (err) throw err;
-
-  fs.readFile(srcFile1, function (err, data) {
-    if (err) throw err;
-
-    fs.appendFile(destFile, data, function (err) {
-      if (err) throw err;
-
-      fs.readFile(srcFile2, function (err, data) {
-        if (err) throw err;
-
-        fs.appendFile(destFile, data, function (err) {
-          if (err) throw err;
-        }); 
-      });
-    });
-  });
-});
+const textA = fs.readFileSync(fileA, 'utf8');
+const textB = fs.readFileSync(fileB, 'utf8');
+fs.writeFileSync(fileC, textA + textB);
